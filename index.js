1ca5920f90ef29c928c88beac9893cd9cf7f9811
sole <sole@mozilla.com>
2014-11-14 10:34:27 +0000

    move parsers out to a module so they can be tested

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ var exec = require('shelljs').exec;
 var async = require('async');
 var FirefoxClient = require('firefox-client');
 var os = process.platform;
-var NETSTAT_CMD = 'netstat -lnptu';
-var LSOF_CMD = 'lsof -i -n -P -sTCP:LISTEN';
+var parsers = require('./lib/parsers');
+var commands = {
+  darwin: 'lsof -i -n -P -sTCP:LISTEN',
+  linux: 'netstat -lnptu'
+};
+
 
 module.exports = discoverPorts;
 
+
 function discoverPorts (opts, callback) {
-  
+
   opts = opts || {};
   var ports = [];
   var search = [];
+  var output;
 
   if (!opts.firefox && !opts.b2g) {
     search = ['firefox', 'b2g'];
@@ -23,37 +29,20 @@ function discoverPorts (opts, callback) {
 
   if (opts.release && opts.release.length > 0) opts.detailed = true;
 
-  /* Commands */
 
-  if (os == 'darwin') {
-    var output = exec(LSOF_CMD, {silent: true}).output;
-    // Example to match
-    // b2g-bin   25779 mozilla   21u  IPv4 0xbbcbf2cee7ddc2a7      0t0  TCP 127.0.0.1:8000 (LISTEN)
-    var regex = new RegExp("^("+ search.join('|') +")(?:-bin)?[\\ ]+([0-9]+).*[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:([0-9]+)");
-    var lines = output.split('\n');
-    lines.forEach(function(line) {
-      var matches = regex.exec(line);
-      if (matches && +matches[3] != 2828) {
-        ports.push({type: matches[1], port: +matches[3], pid: +matches[2]});
-      }
-    });
+  var command = commands[os];
+  var parser = parsers[os];
 
-  } else
-  if (os == 'linux') {
-    var output = exec(NETSTAT_CMD, {silent: true}).output;
-    // Example to match
-    // tcp        0      0 127.0.0.1:6000          0.0.0.0:*              LISTEN      3718/firefox 
-    var regex = new RegExp("tcp.*[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:([0-9]+).*LISTEN[\\ ]+([0-9]+)\\/("+ search.join('|') +")(?:-bin)?");
-    var lines = output.split('\n');
-    lines.forEach(function(line) {
-      var matches = regex.exec(line);
-      if (matches && +matches[1] != 2828) {
-        ports.push({type: matches[3], port: +matches[1], pid: +matches[2],});
-      }
-    });
+  if(parser === undefined) {
+
+    return callback(new Error(os + ' not supported yet'));
 
   } else {
-    return callback(new Error("OS not supported for running"));
+
+    output = exec(command, {silent: true}).output;
+    var lines = output.split('\n');
+    ports = parser(lines, search);
+
   }
 
   if (opts.detailed) {
