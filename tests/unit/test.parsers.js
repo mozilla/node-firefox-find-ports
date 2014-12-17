cc9b026c8d87060784b7a843f002eb0c333677bb
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-12-17 13:38:23 -0500

    Use spaces instead of asphyxiating @sole

--- tests/unit/test.parsers.js
+++ tests/unit/test.parsers.js
@@ -73,14 +73,8 @@ module.exports = {
   noMarionettePortsReturned: function(test) {
 
     var sets = [
-      {
-        output: darwinSimulatorOutput,
-        parser: parsers.darwin
-      },
-      {
-        output: linuxSimulatorOutput,
-        parser: parsers.linux
-      }
+      { output: darwinSimulatorOutput, parser: parsers.darwin },
+      { output: linuxSimulatorOutput, parser: parsers.linux }
       // TODO: Windows
     ];
 
@@ -104,16 +98,8 @@ module.exports = {
   b2gSimulatorPortReturned: function(test) {
 
     var sets = [
-      {
-        output: darwinSimulatorOutput,
-        parser: parsers.darwin,
-        expectedPort: 54637
-      },
-      {
-        output: linuxSimulatorOutput,
-        parser: parsers.linux,
-        expectedPort: 37566
-      }
+      { output: darwinSimulatorOutput, parser: parsers.darwin, expectedPort: 54637 },
+      { output: linuxSimulatorOutput, parser: parsers.linux, expectedPort: 37566 }
       // TODO: Windows
     ];
 
@@ -135,16 +121,8 @@ module.exports = {
   firefoxPortReturned: function(test) {
 
     var sets = [
-      {
-        output: darwinFirefoxOutput,
-        parser: parsers.darwin,
-        expectedPort: 6000
-      },
-      {
-        output: linuxFirefoxOutput,
-        parser: parsers.linux,
-        expectedPort: 6000
-      }
+      { output: darwinFirefoxOutput, parser: parsers.darwin, expectedPort: 6000 },
+      { output: linuxFirefoxOutput, parser: parsers.linux, expectedPort: 6000 }
       // TODO: Windows
     ];
 
@@ -166,14 +144,8 @@ module.exports = {
   // and the array length is 0
   noRuntimeAvailableNoPortReturned: function(test) {
     var sets = [
-      {
-        output: darwinNoRuntimesOutput,
-        parser: parsers.darwin
-      },
-      {
-        output: linuxNoRuntimesOutput,
-        parser: parsers.linux
-      }
+      { output: darwinNoRuntimesOutput, parser: parsers.darwin },
+      { output: linuxNoRuntimesOutput, parser: parsers.linux }
       // TODO: Windows
     ];
 
